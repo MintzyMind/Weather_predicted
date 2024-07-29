e68f835dc43b51bee197851c03b47269099aa18b
Kanthip Raiyai <140625046+MintzyMind@users.noreply.github.com>
2024-07-29 22:36:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,12 @@ st.header('Please up load picture')
 
 #Load Model 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = torch.load('mobilenetv3_large_100_checkpoint_fold0 (1).pt', map_location=device)
+import torch
 
-
+try:
+    model = torch.load('mobilenetv3_large_100_checkpoint_fold0 (1).pt', map_location=device)
+except Exception as e:
+    print(f"Error loading model: {e}")
 
 # Display image & Prediction 
 uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
